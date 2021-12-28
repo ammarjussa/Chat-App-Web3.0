7a40e58a6ced0363f76b0674674e5477802886e4
Ammar Jussa <ammar26497@gmail.com>
2021-12-28 15:33:32 +0500

    Initial code

---
+++ components/Login.js
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (
+    <div className="flex flex-col absolute z-50 h-2/3 w-full items-center justify-center">
+      <p className="text-white text-4xl font-bold">WELCOME TO</p>
+      <p className="text-white text-7xl font-bold">CHAT APP 3.0</p>
+    </div>
+  );
+};
+
+export default Login;
